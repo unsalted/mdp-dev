ae9cb89a219e4cb47da5864b9d706cdc2157774d
unsalted <nick@unsalted.nu>
2014-11-16 16:50:04 -0800

    remove screenxfacextime (will replace with submodule)

--- screenxfacextime/capture-scripts/faceShell.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-screencapture -x -t png ~/Pictures/!face/DESK/DESK-$(date +%Y%m%d%H%M).png
-/usr/local/bin/imagesnap ~/Pictures/!face/FACE/FACE-$(date +%Y%m%d%H%M).png
--- screenxfacextime/genjson.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Generate json file using exiftool
-
- exiftool -json -r -FileName ./assets/images > ./assets/json/index.json
